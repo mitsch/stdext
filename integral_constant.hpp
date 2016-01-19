cd30c0386d251f06783f0d69c52eef6bf2a9f704
Michael Koch <mitsch@mitschlappy>
2016-01-19 17:46:52 +0100

    replaced typed_value by integral_constant

--- integral_constant.hpp
+++ integral_constant.hpp
@@ -1,15 +1,15 @@
-/// @file typed_value.hpp
+/// @file integral_constant.hpp
 /// @author Michael Koch
 /// @copyright CC BY 3.0
 
-#ifndef __STDEXT_TYPED_VALUE_HPP__
-#define __STDEXT_TYPED_VALUE_HPP__
+#ifndef __STDEXT_INTEGRAL_CONSTANT_HPP__
+#define __STDEXT_INTEGRAL_CONSTANT_HPP__
 
 namespace stdext
 {
 
 	/// Typed value
-	template <typename T, T V> struct typed_value
+	template <typename T, T V> struct integral_constant
 	{
 	
 		using value_type = T;
@@ -29,27 +29,27 @@ namespace stdext
 	};
 
 
-	template <bool value> using typed_bool = typed_value<bool, value>;
-	using typed_true = typed_bool<true>;
-	using typed_false = typed_bool<false>;
+	template <bool value> using bool_constant = integral_constant<bool, value>;
+	using true_type = bool_constant<true>;
+	using false_type = bool_constant<false>;
 
 	template <typename ... Ts> struct conjunction;
-	template <> struct conjunction<> : typed_true {};
+	template <> struct conjunction<> : true_type {};
 	template <typename T, typename ... Ts> struct conjunction<T, Ts ...> : _conjunction<T::value, Ts ...> {};
 	template <bool v, typename ... Ts> struct _conjunction;
 	template <typename ... Ts> struct _conjunction<true, Ts ...> : conjunction<Ts ...> {};
-	template <typename ... Ts> struct _conjunction<false, Ts ...> : typed_false {};
+	template <typename ... Ts> struct _conjunction<false, Ts ...> : false_type {};
 	template <typename ... Ts> auto conjunction_v = conjunction<Ts ...>::value;
 
 	template <typename ... Ts> struct disjunction;
-	template <> struct disjunction<> : typed_false {};
+	template <> struct disjunction<> : false_type {};
 	template <typename T, typename ... Ts> struct disjunction<T, Ts ...> : _disjunction<T::value, Ts ...> {};
 	template <bool v, typename ... Ts> struct _disjunction;
-	template <typename ... Ts> struct _disjunction<true, Ts ...> : typed_true {};
+	template <typename ... Ts> struct _disjunction<true, Ts ...> : true_type {};
 	template <typename ... Ts> struct _disjunction<false, Ts ...> : disjunction<Ts ...> {};
 	template <typename ... Ts> auto disjunction_v = disjunction<Ts ...>::value;
 
-	template <typename T> struct negate : typed_bool<not T::value> {};
+	template <typename T> struct negate : bool_constant<not T::value> {};
 	template <typename T> auto negate_v = negate<T>::value;
 
 }
