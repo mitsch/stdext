73497f38f0fe0119b3dfcb029fe5a1bc14314017
Michael Koch <mitsch@mitschlappy>
2016-03-24 17:29:04 +0100

    added reversible bounded sequences

--- sequence_concept.hpp
+++ sequence_concept.hpp
@@ -24,11 +24,10 @@ namespace stdext
 		{
 			S(s);
 			s = S(s);
-			s();
-			{s} -> bool
+			s.decompose();
 		} and
-		is_optional<decltype(std::declval<S>()())>::value and
-		std::is_same<std::tuple_element<1, decltype(std::declval<S>()())::value_type>::type, S>::value;
+		is_optional<decltype(std::declval<S>().decompose())>::value and
+		std::is_same<std::tuple_element<1, decltype(std::declval<S>().decompose())::value_type>::type, S>::value;
 	}
 
 	/// Concept of a bounded sequence with value type \a T
@@ -38,35 +37,22 @@ namespace stdext
 	template <typename S, typename T> concept bool BoundedSequence ()
 	{
 		return BoundedSequence<S> and
-		       std::is_convertible<std::tuple_element<0, decltype(std::declval<S>()())::value_type>::type, T>::value;
+		       std::is_convertible<std::tuple_element<0, decltype(std::declval<S>().decompose())::value_type>::type, T>::value;
 	}
 
-	/// Concept of an indexed bounded sequence
+
+	/// Concept of a reversible bounded sequence
 	///
-	/// An indexed bounded sequence must require the concept of a bounded sequence. All elements in
-	/// the sequence shall be accessible via the index operator. Accessing elements via the index
-	/// operator must return an optional container
-	template <typename S> concept bool IndexedBoundedSequence ()
+	/// A reversible bounded sequence is a bounded sequence which can be folded from behind. It must
+	/// implement the method fold_reverse both for complete folding and conditional folding.
+	template <typename S> concept bool ReversibleBoundedSequence ()
 	{
-		return BoundedSequence<S> and
-		       require (S s){s[std::size_t(0)];} and
-					 require (S s){{s.length()} -> std::size_t;} and
-		       is_optional<decltype(std::declval<S>()[std::size_t(0)])>::value and
-					 std::is_same<
-					 	std::tuple_element<0, decltype(std::declval<S>()())::value_type>::type,
-		       	decltype(std::declval<S>()[std::size_t(0)])::value_type
-						>::value;
+		return BoundedeSequence<S> and
+		       requires(S s){s.decompose_reverse();} and
+					 is_optional_v<decltype(std::declval<S>().decompose_reverse())> and
+		       std::is_same<std::tuple_element<1, decltype()>::type>::value;
 	}
 
-	/// Concept of an indexed unbounded sequence
-	///
-	/// Additionally to the constraints of being an unbounded sequence, type \a S must produce
-	/// elements of type \a T.
-	template <typename S, typename T> concept bool IndexedBoundedSequence ()
-	{
-		return IndexedBoundedSequence<S> and
-		       std::is_convertible<std::tuple_element<0, decltype(std::declval<S>()())::value_type>::type, T>::value
-	}
 
 	/// 
 	template <typename S> concept bool UnboundedSequence ()
@@ -74,7 +60,7 @@ namespace stdext
 		return require (S s)
 		{
 			S(s);
-			s();
+			s.decompose();
 		} and
 		std::is_same<std::tuple_element<1, typename S::value_type>::type, S>::value;
 	}
@@ -85,20 +71,6 @@ namespace stdext
 		       std::is_convertible<std::tuple_element<0, decltype(std::declval<S>()())>::type, T>::value;
 	}
 
-	template <typename S> concept bool IndexedUnboundedSequence ()
-	{
-		return UnboundedSequence<S> and
-		       require(S s){s[std::size_t(0)]} and
-					 std::is_same<
-		       	decltype(std::declval<S>()[std::size_t(0)]),
-		       	std::tuple_element<0, decltype(std::declval<S>()())>::type>::value;
-	}
-
-	template <typename S, typename T> concept bool IndexedUnboundedSequence ()
-	{
-		return IndexedUnboundedSequence<S> and
-		       std::is_convertible<decltype(std::declval<S>()[std::size_t(0)]), T>::value;
-	}
 
 
 	template <typename S> concept bool Sequence ()
@@ -112,18 +84,17 @@ namespace stdext
 	}
 
 
+
 	/// Trait of sequence's element type
-	///
-	/// 
 	template <typename S> struct sequence_type {};
-	template <BoundedSequence S> struct sequence_type<S> :
-		std::tuple_element<0, decltype(std::declval<S>()())::value_type> {};
-	template <UnboundedSequence S> struct sequence_type<S> :
-		std::tuple_element<0, decltype(std::declval<S>()())> {};
+	template <BoundedSequence S> struct sequence_type<S>
+		: std::tuple_element<0, decltype(std::declval<S>()())::value_type> {};
+	template <UnboundedSequence S> struct sequence_type<S>
+		: std::tuple_element<0, decltype(std::declval<S>()())> {};
 	template <typename S> using sequence_type_t = typename sequence_type<S>::type;
 
 
-	/// Folding all elements of a bounded sequence
+	/// Comlete folding of a bounded sequence
 	///
 	/// All elements in \a sequence are folded by \a combiner starting with \a value. This is a
 	/// general implementation which can be specialised for known sequences. The general
@@ -133,148 +104,172 @@ namespace stdext
 	template <typename V, BoundedSequence S, Callable<V, V, sequence_type_t<S>> C>
 	constexpr V fold (C combiner, V value, S sequence)
 	{
-		auto keepFolding = true;
-		while (keepFolding)
+		auto keepOn = true;
+		while (keepOn)
 		{
-			auto decision = decide([&combiner](auto decomposition, auto value, auto sequence)
+			auto decomposition = sequence.decompose();
+			auto decision = decide([&combiner](auto decomposition, V value, S sequence)
 			{
 				value = combiner(std::move(value), std::move(std::get<0>(decomposition));
 				return std::make_tuple(std::move(value), std::move(std::get<1>(decomposition)), true);
-			}, [](auto value, auto sequence)
+			}, [](V value, S sequence)
 			{
 				return std::make_tuple(std::move(value), std::move(sequence), false);
-			}, sequence(), std::move(value), std::move(sequence));
-
+			}, std::move(decomposition), std::move(value), std::move(sequence));
 			value = std::move(std::get<0>(decision));
 			sequence = std::move(std::get<1>(decision));
-			keepFolding = std::get<2>(decision);
+			keepOn = std::get<2>(decision);
 		}
 		return value;
 	}
 
 
-	/// 
+	/// Partial folding of a bounded sequence
+	///
+	/// The elements in \a sequence will be folded over \a value by \a combiner. This is a general
+	/// implementation which decomposes \a sequence element by element and updates \a value with each
+	/// element. The element traversion keeps on as long as \a combiner returns a true flag.The value
+	/// returned with the first false flag from \a combiner will be returned along with a sequence
+	/// holding all the remaining elements of \a sequence including the element for which \a combiner
+	/// has returned the first false flag.
+	///
 	template <typename V, BoundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
 	constexpr std::tuple<V, S> fold (C combiner, V value, S sequence)
-	{
-		bool keepFolding = true;
-		auto onElement = [combiner](auto concatenation, V value, S sequence)
-		{
-			auto nextElement = std::move(std::get<0>(concatenation));
-			auto nextSequence = std::move(std::get<1>(concatenation));
-			auto combination = combiner(std::move(value), std::move(nextElement));
-			value = std::move(std::get<0>(combination));
-			auto keepGoing = std::get<1>(combination);
-			auto returningSequence = keepGoing ? std::move(nextSequence) : std::move(sequence);
-			return std::make_tuple(std::move(value), std::move(returningSequence), keepGoing);
-		};
-		auto onEnd = [](V value, S sequence)
-		{
-			return std::make_tuple(std::move(value), std::move(sequence), false);
-		};
-		while (keepFolding)
-		{
-			auto concatenation = sequence();
-			std::tie(value, sequence, keepFolding) = decide(onElement, onEnd, std::move(concatenation),
-				std::move(value), std::move(sequence));
-		}
-		return std::make_tuple(value, sequence);
-	}
-
-	template <typename V, UnboundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
-	constexpr V fold (C combine, V value, S sequence)
-	{
-		bool keepFolding = true;
-		while (keepFolding)
-		{
-			auto concatenation = sequence();
-			std::tie(value, keepFolding) = combine(std::move(value), std::move(std::get<0>(concatenation)));
-			if (keepFolding)
-				sequence = std::move(std::get<1>(concatenation));
-		}
-		return std::make_tuple(value, sequence);
-	}
-
-	/// Folding all elements of indexed bounded sequence
-	///
-	/// Elements of \a sequence starting at \a index are folded by \a combiner. Initially, folding
-	/// starts off with \a value. All elements from \a index to the end are folded and returned.
-	template <typename V, IndexedBoundedSequence S, Callable<V, V, sequence_type_t<S>> C>
-	constexpr V fold (C combiner, V value, const S & sequence, std::size_t index)
-	{
-		const auto length = sequence.length();
-		for (; index < length; ++index)
-		{
-			value = decide([&](auto element, auto value)
-			{
-				return combiner(std::move(value), std::move(element));
-			}, [](auto value)
-			{
-				return value;
-			}, sequence[index], std::move(value));
-		}
-		return value;
-	}
-
-	/// Folding initial elements of indexed bounded sequence
-	///
-	/// Elements of \a sequence starting at \a index are folded by \a combiner. Initially, folding
-	/// starts off with \a value. As long as \a combiner returns a true flag, folding will continue.
-	/// The returning tuple is the folded value and the first index for which element \a combiner has
-	/// returned a false flag.
-	template <typename V, IndexedBoundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
-	constexpr std::tuple<V, std::size_t> fold (C combiner, V value, const S & sequence, std::size_t index)
-	{
-		const auto length = sequence.length();
-		auto keepOn = true;
-		while (keepOn and index < length)
-		{
-			std::tie(value, keepOn) = decide([&](auto element, auto value)
-			{
-				return combiner(std::move(value), std::move(element));
-			}, [](auto value)
-			{
-				return std::make_tuple(std::move(value), true);
-			}, sequence[index], std::move(value));
-			if (keepOn) ++index;
-		}
-		return std::make_tuple(std::move(value), index);
-	}
-
-	/// Folding initial elements of indexed unbounded sequence
-	///
-	/// Elements of \a sequence starting at \a index are folded by \a combiner. Initially, folding
-	/// starts off with \a value. As long as \a combiner returns a true flag, folding will continue.
-	/// The returning tuple is the folded value and the first index for which element \a combiner has
-	/// returned a false flag.
-	template <typename V, IndexedUnboundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
-	constexpr std::tuple<V, std::size_t> fold (C combiner, V value, const S & sequence, std::size_t index)
 	{
 		auto keepOn = true;
 		while (keepOn)
 		{
-			std::tie(value, keepOn) = combiner(std::move(value), sequence[index]);
-			if (keepOn) ++index;
+			auto decomposition = sequence.decompose();
+			auto decision = decide([&combiner](auto decomposition, V value, S sequence)
+			{
+				auto combination = combiner(std::move(value), std::move(std::get<0>(decomposition));
+				auto nextSequence = std::get<1>(combination) ? std::move(std::get<1>(decomposition)) : std::move(sequence));
+				return std::make_tuple(std::move(std::get<0>(combination)), std::move(nextSequence), std::get<1>(combination));
+			}, [](V value, S sequence)
+			{
+				return std::make_tuple(std::move(value), std::move(sequence), false);
+			}, std::move(decomposition), std::move(value), std::move(sequence));
+			value = std::move(std::get<0>(decision));
+			sequence = std::move(std::get<1>(decision));
+			keepOn = std::get<2>(decision);
 		}
-		return std::make_tuple(std::move(value), index);
+		return std::make_tuple(value, sequence);
 	}
 
 
-	template <typename S>
-		requires BoundedSequence<S>
+	/// Partial folding of an unbounded sequence
+	///
+	/// The elements in \a sequence will be folded over \a value by \a combiner. This is a general
+	/// implementation which decomposes \a sequence element by element and updates \a value with each
+	/// element. The element traversion keeps on as long as \a combiner returns a true flag. The
+	/// value returned with the first false flag from \a combiner will be returned along with a
+	/// sequence holding all the remaining elements of \a sequence.
+	///
+	template <typename V, UnboundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
+	constexpr std::tuple<V, S> fold (C combiner, V value, S sequence)
+	{
+		bool keepFolding = true;
+		while (keepFolding)
+		{
+			auto decomposition = sequence.decompose();
+			std::tie(value, keepFolding) = combiner(std::move(value), std::move(std::get<0>(decomposition)));
+			if (keepFolding)
+				sequence = std::move(std::get<1>(decomposition));
+		}
+		return std::make_tuple(value, sequence);
+	}
+
+
+	/// Complete reverse folding of a bounded sequence
+	///
+	/// The elements in \a sequence will be folded in reverse over \a value by \a combiner. This is a
+	/// general implementation which decomposes \a sequence in reverse element by element and updates
+	/// \a value with each element.
+	///
+	template <typename V, ReversibleBoundedSequence S, Callable<V, V, sequence_type_t<S>> C>
+	constexpr V fold_reverse (C combiner, V value, S sequence)
+	{
+		auto keepOn = true;
+		while (keepOn)
+		{
+			auto decomposition = sequence.decompose_reverse();
+			auto decision = decide([&combiner](auto decomposition, V value, S sequence)
+			{
+				value = combiner(std::move(value), std::move(std::get<0>(decomposition));
+				return std::make_tuple(std::move(value), std::move(sequence), true);
+			}, [](V value, S sequence)
+			{
+				return std::make_tuple(std::move(value), std::move(sequence), false);
+			}, std::move(decomposition), std::move(value), std::move(sequence));
+			value = std::move(std::get<0>(decision));
+			sequence = std::move(std::get<1>(decision));
+			keepOn = std::get<2>(decision);
+		}
+		return value;
+	}
+
+
+	/// Partial reverse folding of a bounded sequence
+	///
+	/// The elements in \a sequence will be folded in reverse over \a value by \a combiner. This is a
+	/// general implementaiton which decomposes \a sequence in reverse element byelement and updates
+	/// \a value with each element. The element traversion keeps on as long as \a combiner returns a
+	/// true flag. The value returned with the first false flag from \a combiner will be returned
+	/// along with a sequence holding all the remaining elements of \a sequence.
+	///
+	template <typename V, ReversibleBoundedSequence S, Callable<std::tuple<V, bool>, V, sequence_type_t<S>> C>
+	constexpr std::tuple<V, S> fold_reverse (C combiner, V value, S sequence)
+	{
+		auto keepOn = true;
+		while (keepOn)
+		{
+			auto decomposition = sequence.decompose_reverse();
+			auto decision = decide([&combiner](auto decomposition, V value, S sequence)
+			{
+				auto combination = combiner(std::move(value), std::move(std::get<0>(decomposition)));
+				auto nextSequence = std::get<1>(combination) ? std::move(std::get<1>(decomposition)) : std::move(sequence);
+				return std::make_tuple(std::move(std::get<0>(combination)), std::move(nextSequence), std::get<1>(combination));
+			}, [](V value, S sequence)
+			{
+				return std::make_tuple(std::move(value), std::move(sequence), false);
+			}, std::move(decomposition), std::move(value), std::move(sequence));
+			value = std::move(std::get<0>(decision));
+			sequence = std::move(std::get<1>(decision));
+			keepOn = std::get<2>(decision);
+		}
+		return std::make_tuple(std::move(value), std::move(sequence));
+	}
+
+
+	/// Emptiness test for bounded sequence
+	///
+	/// The bounded \a sequence will be tested on having no element left, aka being empty. This is a
+	/// general implementation, which tries to decompose and tests whether the returning optional
+	/// container is empty or not. The method can be overloaded for more efficient and more
+	/// performant implementation of particular sequences.
+	///
+	template <BoundedSequence S>
+	constexpr bool empty (const S & sequence)
+	{
+		const auto decomposition = sequence.decompose();
+		return not decomposition.good();
+	}
+
+
+	/// Length of bounded sequence
+	///
+	/// The length of the bounded \a sequence will be computed. This is a general implementation
+	/// which computes the length by folding over its elements while counting up. The method can be
+	/// overloaded for more efficient and more performant implementation of particular sequences.
+	///
+	template <BoundedSequence S>
 	constexpr std::size_t length (S sequence)
 	{
-		return fold([](auto counter, auto value)
+		return fold([](auto counter, auto element)
 		{
+			(void)element;
 			return counter + 1;
 		}, std::size_t(0), std::move(sequence));
-	}
-
-	template <typename S>
-		requires IndexedBoundedSequence<S>
-	constexpr std::size_t length (S sequence)
-	{
-		return s.length();
 	}
 
 }
