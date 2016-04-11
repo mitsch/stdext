a5a77bb8f0ec6946c710e7ff4cf49ad9121fc1cf
Michael Koch <mitsch@mitschlappy>
2016-04-11 10:26:40 +0200

    redesign

--- utf8_view.hpp
+++ utf8_view.hpp
@@ -16,290 +16,216 @@ namespace stdext
 	class utf8_view
 	{
 	
-		private:
+	private:
 
-			/// The underlying encoding byte sequence
-			array_view<const unsigned char> encoding;
-			
+		/// The underlying encoding byte sequence
+		array_view<const unsigned char> encoding;
 
-			/// Forward character decoding
-			static constexpr std::tuple<char32_t, std::size_t> decode (const unsigned char * start);
+		/// Forward character decoding
+		static constexpr std::tuple<char32_t, std::size_t> decode (const unsigned char * start);
 
-			/// Backward character decoding
-			static constexpr std::tuple<char32_t, std::size_t> decode_reverse (const unsigned char * start, std::size_t length);
+		/// Backward character decoding
+		static constexpr std::tuple<char32_t, std::size_t> decode_reverse (const unsigned char * start, std::size_t length);
 
-		public:
+	public:
 
+		explicit constexpr utf8_view (array_view<const unsigned char> encoding)
+			: encoding(encoding)
+			noexcept
+		{}
 
-			/// Attribute constructor
-			explicit constexpr utf8_view (array_view<const unsigned char> encoding)
-				: encoding(encoding)
-				noexcept
-			{}
+		constexpr bool empty () const
+		{
+			return encoding.empty();
+		}
 
-			
-
-
-
-			/// Emptiness
-			///
-			/// The view will be tested on having no characters left. The behaviour is identical to
-			/// calling \code not *this \endcode.
-			///
-			/// @note Time complexity is constant.
-			///
-			constexpr bool empty () const
+		constexpr size_t length () const
+		{
+			return encoding.fold([](auto count, auto character)
 			{
-				return bytes.empty();
+				return count + (character & 0b11000000 == 0b10000000) ? 0 : 1;
+			}, size_t(0));
+		}
+
+		constexpr size_t raw_length () const
+		{
+			return encoding.length();
+		}
+
+		constexpr const unsigned char * raw_data () const
+		{
+			return encoding.data();
+		}
+
+		constexpr array_view<const unsigned char> raw_view () const
+		{
+			return encoding;
+		}
+
+		constexpr optional<std::tuple<char32_t, utf8_view>> decompose_prefix () const
+		{
+			return make_optional(encoding.length() > 0, [&encoding]()
+			{
+				const auto decoding = decode(encoding.data());
+				const auto remainingLength = encoding.length() - std::get<1>(decoding);
+				const auto offsetData = encoding.data() + std::get<1>(decoding);
+				const auto remainings = utf8_view(array_view<const unsigned char>(offsetData, remainingLength));
+				return std::make_tuple(std::ge<0>(decoding), remainings);
+			});
+		}
+		
+		constexpr optional<std::tuple<char32_t, utf8_view>> decompose_suffix () const
+		{
+			return make_optional(encoding.length() > 0, [&]()
+			{
+				const auto decoding = decode_reverse(encoding.data(), encoding.length());
+				const auto remainingLength = encoding.length() - std::get<1>(decoding);
+				const auto remainings = utf8_view(array_view<const unsigned char>(encoding.data(), remainingLength));
+				return std::make_tuple(std::get<0>(decoding), remainings);
+			});
+		}
+		
+		template <typename V, Callable<V, V, char32_t> C>
+		constexpr V fold (C combiner, V value) const
+		{
+			std::size_t index = 0;
+			auto remainingLength = encoding.length();
+			while (remainingLength > 0)
+			{
+				const auto decoding = decode(encoding.data() + index);
+				value = combiner(std::move(value), std::get<0>(decoding));
+				index += std::get<1>(decoding);
+				remainingLength -= std::get<1>(decoding);
 			}
+			return value;
+		}
 
-			/// Not emptiness
-			///
-			/// The view will be tested on having at least one character left. The behaviour is identical
-			/// to calling \code not this->empty() \endcode.
-			///
-			/// @note Time complexity is constant.
-			///
-			constexpr bool operator () const
+		template <typename V, Callable<V, V, char32_t> C>
+		constexpr V fold_reverse (C combiner, V value) const
+		{
+			auto remainingLength = encoding.length();
+			while (remainingLength > 0)
 			{
-				return not bytes.empty();
+				const auto decoding = decode_reverse(encoding.data(), remainingLength);
+				value = combiner(std::move(value), std::get<0>(decoding));
+				remainingLength -= std::get<1>(decoding);
 			}
+			return value;
+		}
 
-			/// Length in characters
-			///
-			/// The amount of characters encoded in the view will be returned. The amount of characters
-			/// will be equal or less the amount of underlying bytes. The amount of characters will not
-			/// be less than a fourth of the amount of underlying bytes.
-			///
-			/// @note Time complexity is linear to the length of the view.
-			///
-			constexpr std::size_t length () const
+		template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
+		constexpr std::tuple<V, utf8_view, utf8_view> fold (C combiner, V value) const
+		{
+			auto keepOn = true;
+			std::size_t index = 0;
+			auto remainingLength = encoding.length();
+			while (remainingLength > 0 and keepOn)
 			{
-				return fold([](auto count, auto byte)
+				const auto decoding = decode(encoding.data() + index);
+				std::tie(value, keepOn) = combiner(std::move(value), std::get<0>(decoding));
+				if (keepOn)
 				{
-					return count + (byte & 0xc0 == 0x80) ? 0 : 1;
-				}, std::size_t(0));
-			}
-
-			/// Length in bytes
-			///
-			/// The amount of encoding bytes does not necessarily refers to the amount of encoded
-			/// characters. The amount of bytes is at most four times and at least the same amount of
-			/// encoded characters.
-			///
-			/// @note Time complexity is constant.
-			/// 
-			constexpr std::size_t raw_length () const
-			{
-				return bytes.length();
-			}
-
-			/// Data access to bytes
-			///
-			/// A pointer to the first encoding byte will be returned. The range of encoding bytes will
-			/// start at the returned pointer and will reach for \code raw_length() \endcode bytes. If
-			/// the view is empty, the returning pointer might be a null pointer.
-			///
-			/// @note Time complexity is constant.
-			///
-			constexpr const unsigned char * raw_data () const
-			{
-				return bytes.data();
-			}
-
-			/// View on encoding bytes
-			///
-			/// A view onto the encoding byte sequence will be returned. The state of the returning view
-			/// will be equivalent to calling
-			/// \code array_view<const unsigned char*>(this->raw_data(), this->raw_length()) \endcode.
-			///
-			/// @note Time complexity is constant.
-			///
-			constexpr array_view<const unsigned char*> raw_view () const
-			{
-				return bytes;
-			}
-
-
-
-
-
-
-			/// Prefix decomposing
-			///
-			/// If the view is not empty, the next character will be decoded. Along with the decoded
-			/// character, a view on the succeeding encodes will be returned. If the view is empty, an
-			/// empty optional container will be returned.
-			///
-			constexpr optional<std::tuple<char32_t, utf8_view>> decompose_prefix () const
-			{
-				return make_optional(bytes.length() > 0, [&]()
-				{
-					const auto decoding = decode(bytes.data());
-					const auto remainingLength = bytes.length() - std::get<1>(decoding);
-					const auto offsetData = bytes.data() + std::get<1>(decoding);
-					const auto remainings = utf8_view(array_view<const unsigned char>(offsetData, remainingLength));
-					return std::make_tuple(std::get<0>(decoding), remainings);
-				});
-			}
-
-			/// Suffix decomposing
-			///
-			/// If the view is not empty, the last character will be decoded. Along with the decoded
-			/// character, a view on the preceeding encodes will be returned. If the view is empty, an
-			/// empty optional container will be returned.
-			///
-			constexpr optional<std::tuple<char32_t, utf8_view>> decompose_suffix () const
-			{
-				return make_optional(bytes.length() > 0, [&]()
-				{
-					const auto decoding = decode_reverse(bytes.data(), bytes.length());
-					const auto remainingLength = bytes.length() - std::get<1>(decoding);
-					const auto remainings = utf8_view(array_view<const unsigned char>(bytes.data(), remainingLength));
-					return std::make_tuple(std::get<0>(decoding), remainings);
-				});
-			}
-
-			/// Prefix decomposing
-			///
-			/// The behaviour is identical to calling decompose_prefix(). The method allows to handle
-			/// utf8_view as a bounded sequence.
-			///
-			constexpr optional<std::tuple<char32_t, utf8_view>> operator () () const
-			{
-				return decompose_prefix();
-			}
-
-
-
-
-			/// Forward folding
-			///
-			/// The encoded characters will be folded with \a combiner over the initial \a value. The order
-			/// of characters passed on to \a combiner will be forwards starting with the first one and
-			/// ending with the last one in the view. The folded value will be returned.
-			///
-			/// @param combiner  Callable object which takes a value and a character and will return a
-			///                  folding of both
-			/// @param value     Initial value to start the folding with
-			/// 
-			template <typename V, Callable<V, V, char32_t> C>
-			constexpr V fold (C combiner, V value) const
-			{
-				std::size_t index = 0;
-				auto remainingLength = bytes.length();
-				while (remainingLength > 0)
-				{
-					const auto decoding = decode(bytes.data() + index);
-					value = combiner(std::move(value), std::get<0>(decoding));
 					index += std::get<1>(decoding);
 					remainingLength -= std::get<1>(decoding);
 				}
-				return value;
 			}
+			const auto unfolded = utf8_view(encoding.data() + index, remainingLength);
+			const auto folded = utf8_view(encoding.data(), index);
+			return std::make_tuple(std::move(value), unfolded, folded);
+		}
 
-			/// Backward folding
-			template <typename V, Callable<V, V, char32_t> C>
-			constexpr V fold_reverse (C combiner, V value) const
+		template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
+		constexpr std::tuple<V, utf8_view, utf8_view> fold_reverse (C combiner, V value) const
+		{
+			auto keepOn = true;
+			auto remainingLength = encoding.length();
+			while (remainingLength > 0 and keepOn)
 			{
-				auto remainingLength = bytes.length();
-				while (remainingLength > 0)
+				const auto decoding = decode_reverse(encoding.data(), remainingLength);
+				std::tie(value, keepOn) = combiner(std::move(value), std::get<0>(decoding));
+				if (keepOn)
 				{
-					const auto decoding = decode_reverse(bytes.data(), remainingLength);
-					value = combiner(std::move(value), std::get<0>(decoding));
 					remainingLength -= std::get<1>(decoding);
 				}
-				return value;
 			}
-
-			/// Conditional forward folding
-			template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
-			constexpr std::tuple<V, utf8_view> fold (C combiner, V value) const
-			{
-				auto keepOn = true;
-				std::size_t index = 0;
-				auto remainingLength = bytes.length();
-				while (remainingLength > 0 and keepOn)
-				{
-					const auto decoding = decode(bytes.data() + index);
-					std::tie(value, keepOn) = combiner(std::move(value), std::get<0>(decoding));
-					if (keepOn)
-					{
-						index += std::get<1>(decoding);
-						remainingLength -= std::get<1>(decoding);
-					}
-				}
-				return std::make_tuple(std::move(value), utf8_view(bytes.data() + index, remainingLength));
-			}
-
-			/// Conditional backward folding
-			template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
-			constexpr V fold_reverse (C combiner, V value) const
-			{
-				auto keepOn = true;
-				auto remainingLength = bytes.length();
-				while (remainingLength > 0 and keepOn)
-				{
-					const auto decoding = decode_reverse(bytes.data(), remainingLength);
-					std::tie(value, keepOn) = combiner(std::move(value), std::get<0>(decoding));
-					if (keepOn)
-					{
-						remainingLength -= std::get<1>(decoding);
-					}
-				}
-				return std::make_tuple(std::move(value), utf8_view(bytes.data(), remainingLength));
-			}
+			const auto unfolded = utf8_view(encoding.data(), remainingLength);
+			const auto folded = utf8_view(encoding.data() + remainingLength, encoding.length() - remainingLength);
+			return std::make_tuple(std::move(value), unfolded, folded);
+		}
 			
 
 
 
-			/// Prefix splitting with amount of characters
-			///
-			/// The view will be splitted into two partitions. The first partition will hold at most \a
-			/// count characters. The second partition will hold all succeeding characters. If the view
-			/// holds less than \a count characters, the first partition will be equivalent to the
-			/// original view and the second partition will be empty. Along with both partition, the
-			/// amount of characters held in the first partition will be returned.
-			///
-			/// @note Time complexity is linear to the length of the view.
-			///
-			constexpr std::tuple<utf8_view, utf8_view, std::size_t> split_prefix (std::size_t count) const;
+		/// Prefix splitting with amount of characters
+		///
+		/// The view will be splitted into two partitions. The first partition will hold at most \a
+		/// count characters. The second partition will hold all succeeding characters. If the view
+		/// holds less than \a count characters, the first partition will be equivalent to the
+		/// original view and the second partition will be empty. Along with both partition, the
+		/// amount of characters held in the first partition will be returned.
+		///
+		/// @note Time complexity is linear to the length of the view.
+		///
+		constexpr std::tuple<utf8_view, utf8_view, size_t> split_prefix (size_t count) const
+		{
+			const auto folding = encoding.fold([](const auto count, const auto character)
+			{
+				const auto isHead = (character & 0b11000000) != 0b10000000;
+				const auto keepOn = count > 0 or not isHead;
+				const auto newCount = isHead and count > 0 ? count - 1 : count;
+				return std::make_tuple(newCount, keepOn);
+			}, count);
+			const auto pre = utf8_view(std::get<2>(folding));
+			const auto post = utf8_view(std::get<1>(folding));
+			return std::make_tuple(pre, post, count - std::get<0>(folding));
+		}
 
-			/// Prefix splitting with predictor
-			///
-			/// The view will be splitted into two partitions. The first partition will hold the longest
-			/// prefix of characters for which \a predictor returns true. The second partition will hold
-			/// all succeeding characters. Characters are passed on starting with the first and
-			/// traversing through the last one in the view.
-			///
-			/// @note Time complexity is linear to the length of the view.
-			///
-			template <Callable<bool, char32_t> C>
-			constexpr std::tuple<utf8_view, utf8_view> split_prefix (C predictor) const;
+		/// Prefix splitting with predictor
+		///
+		/// The view will be splitted into two partitions. The first partition will hold the longest
+		/// prefix of characters for which \a predictor returns true. The second partition will hold
+		/// all succeeding characters. Characters are passed on starting with the first and
+		/// traversing through the last one in the view.
+		///
+		/// @note Time complexity is linear to the length of the view.
+		///
+		template <Callable<bool, char32_t> C>
+		constexpr std::tuple<utf8_view, utf8_view, size_t> split_prefix (C predictor) const
+		{
+			const auto folding = encoding.fold([predictor=std::move(predictor)](const auto count, const auto character)
+			{
+				const auto prediction = predictor(character);
+				const auto newCount = count + prediction ? 1 : 0;
+				return std::make_tuple(newCount, prediction);
+			}, size_t(0));
+			const auto pre = utf8_view(std::get<2>(folding));
+			const auto post = utf8_view(std::get<1>(folding));
+			return std::make_tuple(pre, post, std::get<0>(folding));
+		}
 
-			/// Prefix splitting with predictor and variable
-			///
-			/// The view will be splitted into two partitions. The first partition will hold the longest
-			/// prefix of characters for which \a predictor returns true. The second partition will hold
-			/// all succeeding characters. Characters are passed on starting with the first and
-			/// traversing through the last one in the view.
-			///
-			/// @note Time complexity is linear to the length of the view.
-			///
-			template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
-			constexpr std::tuple<utf8_view, utf8_view, V> split_prefix (C predictor, V variable) const;
+		/// Prefix splitting with predictor and variable
+		///
+		/// The view will be splitted into two partitions. The first partition will hold the longest
+		/// prefix of characters for which \a predictor returns true. The second partition will hold
+		/// all succeeding characters. Characters are passed on starting with the first and
+		/// traversing through the last one in the view.
+		///
+		/// @note Time complexity is linear to the length of the view.
+		///
+		template <typename V, Callable<std::tuple<V, bool>, V, char32_t> C>
+		constexpr std::tuple<utf8_view, utf8_view, V> split_prefix (C predictor, V variable) const;
 
-			/// Prefix splitting with unicode sequence
-			///
-			/// The view will be splitted into two partitions. The first partition will hold the longest
-			/// prefix of characters which \a other shares with this view. The second partition will hold
-			/// all the succeeding characters in the view. Both partitions will be returned along with a
-			/// sequence holding all remaining elements of \a other.
-			///
-			/// @note Time complexity is linear to the length of the view.
-			///
-			template <Sequence<char32_t> S>
-			constexpr std::tuple<utf8_view, utf8_view, S> split_prefix (S other) const;
+		/// Prefix splitting with unicode sequence
+		///
+		/// The view will be splitted into two partitions. The first partition will hold the longest
+		/// prefix of characters which \a other shares with this view. The second partition will hold
+		/// all the succeeding characters in the view. Both partitions will be returned along with a
+		/// sequence holding all remaining elements of \a other.
+		///
+		/// @note Time complexity is linear to the length of the view.
+		///
+		template <Sequence<char32_t> S>
+		constexpr std::tuple<utf8_view, utf8_view, S> split_prefix (S other) const;
 
 
 
